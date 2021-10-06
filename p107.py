5c9f5408bfaf2736bce091b96d1f62938f6f3053
raama-coder <72999673+raama-coder@users.noreply.github.com>
2021-10-06 18:52:38 -0400

    Delete p107.py

--- p107.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-import csv
-import plotly.express as px
-df = pd.read_csv("/Users/raama/Documents/White hat jr./python/p107/data.csv")
-mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
-fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt",title="Data Analysis Of Level to Attempt")
-fig.show()
